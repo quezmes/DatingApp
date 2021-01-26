3df8163d5cbf4a89cc5da0a75bdb61ee92ae75c2
quezmes <quezmes@gmail.com>
2021-01-26 15:33:11 +0100

    End of Section 4

---
+++ API/DTOs/UserDTO.cs
@@ -0,0 +1,8 @@
+namespace DatingApp.API.DTOs
+{
+    public class UserDTO
+    {
+        public string UserName { get; set; }
+        public string Token { get; set; }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,8 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using API.Data;
+using DatingApp.API.Extensions;
+using DatingApp.API.Interfaces;
+using DatingApp.API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,6 +17,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace API
@@ -22,7 +28,7 @@ namespace API
         public Startup(IConfiguration config)
         {
             _config = config;
-        
+
         }
 
         // public IConfiguration Configuration { get; }
@@ -30,12 +36,11 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(options =>
-            {
-                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
+            services.AddApplicationServices(_config);
             services.AddControllers();
             services.AddCors();
+            services.AddIdentityServices(_config);
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -57,6 +62,7 @@ namespace API
             app.UseRouting();
 
             app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
